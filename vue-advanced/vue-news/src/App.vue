b94d9f08df6dfc3d5a37fe40c4012e04023aeeb7
강민희 <queen.minhee@gmail.com>
2022-04-01 14:47:08 +0900

    all files uploaded

---
+++ vue-advanced/vue-news/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+	<!-- url이 만약 news라면 newsComponent-->
+	<div id="app">
+		<tool-bar></tool-bar>
+		<router-view></router-view>
+	</div>
+</template>
+
+<script>
+import ToolBar from '@/components/ToolBar';
+
+export default {
+	components: {
+		ToolBar,
+	},
+};
+</script>
+
+<style>
+body {
+	padding: 0;
+	margin: 0;
+}
+</style>
