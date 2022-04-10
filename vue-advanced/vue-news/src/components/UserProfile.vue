58cc43bcc64b4dd8b499f8f87e5e6172afb126a5
강민희 <leefree3@daou.co.kr>
2022-04-11 07:55:27 +0900

    ~리팩토링2 (진행중)

---
+++ vue-advanced/vue-news/src/components/UserProfile.vue
@@ -0,0 +1,44 @@
+<template>
+	<div class="user-container">
+		<div class="user-container"><i class="fas fa-user"></i></div>
+		<div class="user-description">
+			<div>
+				{{ info.id }}
+			</div>
+			<div class="time">
+				{{ info.created }}
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'UserProfile',
+	props: {
+		info: Object,
+	},
+	// computed: {
+	// 	userInfo() {
+	// 		return this.$store.state.user;
+	// 	},
+	// },
+};
+</script>
+
+<style scoped>
+.user-container {
+	display: flex;
+	align-items: center;
+	padding: 0.5rem;
+}
+.fa-user {
+	font-size: 2.5rem;
+}
+.user-description {
+	padding-left: 8px;
+}
+.time {
+	font-size: 0.7rem;
+}
+</style>
--- vue-advanced/vue-news/src/views/UserView.vue
+++ vue-advanced/vue-news/src/views/UserView.vue
@@ -1,14 +1,19 @@
 <template>
 	<div>
-		<p>name: {{ userInfo.id }}</p>
-		<p>karma: {{ userInfo.karma }}</p>
-		<p>created: {{ userInfo.created }}</p>
+		<user-profile :info="userInfo"></user-profile>
+		<!--		<p>name: {{ userInfo.id }}</p>-->
+		<!--		<p>karma: {{ userInfo.karma }}</p>-->
+		<!--		<p>created: {{ userInfo.created }}</p>-->
 	</div>
 </template>
 
 <script>
+import UserProfile from '@/components/UserProfile';
 export default {
 	name: 'UserView',
+	components: {
+		UserProfile,
+	},
 	computed: {
 		userInfo() {
 			return this.$store.state.user;
