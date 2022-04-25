159b1da4cd86811448815cfdbc4480136bdb9646
강민희 <queen.minhee@gmail.com>
2022-04-25 23:26:57 +0900

    ~ router navigation

--- vue-advanced/vue-news/src/router/index.js
+++ vue-advanced/vue-news/src/router/index.js
@@ -6,6 +6,8 @@ import JobViews from '@/views/JobViews';
 import UserView from '@/views/UserView';
 import ItemView from '@/views/ItemView';
 import createListView from '@/views/CreateListView';
+import bus from '@/utils/bus';
+import { store } from '@/store';
 
 Vue.use(VueRouter);
 
@@ -21,6 +23,19 @@ export const router = new VueRouter({
 			name: 'news',
 			component: NewsView,
 			// component: createListView('NewsView'),
+			beforeEnter: (to, from, next) => {
+				bus.$emit('start:spinner');
+				store
+					.dispatch('FETCH_LIST', to.name)
+					.then(() => {
+						console.log('fetched');
+						bus.$emit('end:spinner');
+						next();
+					})
+					.catch(error => {
+						console.log(error);
+					});
+			},
 		},
 		{
 			path: '/ask',
