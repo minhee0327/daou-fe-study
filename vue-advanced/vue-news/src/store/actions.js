01e754db9d1608497c1eeabd1ad1068e1ae1ab00
강민희 <queen.minhee@gmail.com>
2022-04-29 23:51:03 +0900

    ~ async-await 과제

--- vue-advanced/vue-news/src/store/actions.js
+++ vue-advanced/vue-news/src/store/actions.js
@@ -5,9 +5,13 @@ import {
 	fetchItem,
 	fetchUserInfo,
 	fetchList,
+	fetchNewsList,
+	fetchAskList,
+	fetchJobsList,
 } from '@/api';
 
 export default {
+	// promise
 	// FETCH_NEWS(context) {
 	// 	return fetchNewsList()
 	// 		.then(response => context.commit('SET_NEWS', response.data))
@@ -24,6 +28,24 @@ export default {
 	// 		.then(({ data }) => commit('SET_ASKS', data))
 	// 		.then(error => console.log(error));
 	// },
+
+	// async
+	async FETCH_NEWS(context) {
+		const response = await fetchNewsList();
+		context.commit('SET_NEWS', response.data);
+		return response;
+	},
+	async FETCH_ASKS(context) {
+		const response = await fetchAskList();
+		context.commit('SET_ASKS', response.data);
+		return response;
+	},
+	async FETCH_JOBS(context) {
+		const response = await fetchJobsList();
+		context.commit('SET_JOBS', response.data);
+		return response;
+	},
+
 	FETCH_USER({ commit }, name) {
 		return fetchUserInfo(name)
 			.then(({ data }) => commit('SET_USER', data))
