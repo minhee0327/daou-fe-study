ed214c45a5858d2c986eec0479e23bdbb03a3845
강민희 <queen.minhee@gmail.com>
2023-06-05 09:56:17 +0900

    typescript 기본

---
+++ learn-typescript/class-note/1_type-basic.ts
@@ -0,0 +1,32 @@
+//js 문자열
+// let str = 'hello';
+
+//ts 문자열 선언
+let str: string = 'hello';
+
+//ts 숫자
+let num: number = 10;
+
+//ts 배열
+let arr: Array<number> = [1, 2, 3];
+// let heroes: Array<string> = ['Capt', 'Thor', 'Hulk', 10];
+let items: number[] = [1, 2, 3];
+
+//TS 튜플 (특정 인덱스에 어떤 타입을 사용할지 정의가 된 배열)
+let address: [string, number] = ['gangnam', 1];
+
+//TS 객체
+let obj: object = {};
+// let person: object = {
+//     name: 'capt',
+//     age: 10,
+// };
+let person: { name: string, age: number } = {
+    name: 'thor',
+    age: 10000,
+}
+
+//TS 진위값
+let show: boolean = true
+
+
