ad2cd24e40e7faf2674615c66721fb9732753ec0
강민희 <queen.minhee@gmail.com>
2023-06-08 14:00:18 +0900

    TODO 앱 조회,삭제 기능 추가

--- learn-vue-typescript/vue-todo/src/App.vue
+++ learn-vue-typescript/vue-todo/src/App.vue
@@ -1,23 +1,55 @@
 <template>
   <div>
-    <h1>Vue Todo with Typescript</h1>
-    <TodoInput
-      :item="todoText"
-      @input="updateTodoText"
-      @add="addTodoItem"
-    ></TodoInput>
+    <header>
+      <h1>Vue Todo with Typescript</h1>
+    </header>
+    <main>
+      <TodoInput
+        :item="todoText"
+        @input="updateTodoText"
+        @add="addTodoItem"
+      ></TodoInput>
+      <div>
+        <ul>
+          <TodoListItem
+            v-for="(todoItem, index) in todoItems"
+            :key="index"
+            :index="index"
+            :todoItem="todoItem"
+            @remove="removeTodoItem"
+          ></TodoListItem>
+        </ul>
+      </div>
+    </main>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import TodoInput from "@/components/TodoInput.vue";
+import TodoListItem from "@/components/TodoListItem.vue";
+
+const STORAGE_KEY = "vue-todo-ts-v1";
+const storage = {
+  save(todoItems: any[]) {
+    const parsed = JSON.stringify(todoItems);
+    localStorage.setItem(STORAGE_KEY, parsed);
+  },
+  fetch() {
+    const todoItems = localStorage.getItem(STORAGE_KEY) || "[]";
+    return JSON.parse(todoItems);
+  },
+};
 
 export default Vue.extend({
-  components: { TodoInput },
+  components: { TodoListItem, TodoInput },
+  created() {
+    this.fetchTodoItems();
+  },
   data() {
     return {
       todoText: "",
+      todoItems: [] as any,
     };
   },
   methods: {
@@ -26,11 +58,19 @@ export default Vue.extend({
     },
     addTodoItem() {
       const value = this.todoText;
-      localStorage.setItem(value, value);
+      this.todoItems.push(value);
+      storage.save(this.todoItems);
       this.initTodoText();
     },
     initTodoText() {
       this.todoText = "";
+    },
+    fetchTodoItems() {
+      this.todoItems = storage.fetch();
+    },
+    removeTodoItem(index: number) {
+      this.todoItems.splice(index, 1);
+      storage.save(this.todoItems);
     },
   },
 });
---
+++ learn-vue-typescript/vue-todo/src/components/TodoListItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <li>
+    <span>{{ todoItem }}</span>
+    <button @click="removeItem">삭제</button>
+  </li>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    todoItem: {
+      type: String,
+    },
+    index: {
+      type: Number,
+    },
+  },
+  methods: {
+    removeItem() {
+      this.$emit("remove", this.index);
+    },
+  },
+});
+</script>
+
+<style scoped></style>
