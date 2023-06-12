da1ae6bed5d41e3005b717ae132f7fe177d55226
강민희 <queen.minhee@gmail.com>
2023-06-12 17:56:12 +0900

    nuxt.js uploaded

---
+++ nuxt-sample/pages/about.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      <h2>About</h2>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "about"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ nuxt-sample/pages/product/temp.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    temp
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Temp"
+}
+</script>
+
+<style scoped>
+
+</style>
