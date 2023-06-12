1136d76e646dff0aa4dab238f882a8fcd6af5543
강민희 <queen.minhee@gmail.com>
2023-06-12 17:55:29 +0900

    nuxt

---
+++ coupop-sample/layouts/default.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div>
+      <header>
+        <nav>
+          <NuxtLink to="/">
+            Nuxt Dojo
+          </NuxtLink>
+          <ul>
+            <li>
+              <NuxtLink to="/">
+                Home
+              </NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/about">
+                About
+              </NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/product">
+                Product
+              </NuxtLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </div>
+
+    <div>
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.router-link-exact-active{
+  color: #12b488;
+}
+</style>
---
+++ coupop-sample/pages/product/[id].vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+  </div>
+</template>
+
+<script setup>
+  import { useRoute } from "nuxt/app";
+
+  const {id} = useRoute().params
+</script>
+
+<style scoped>
+
+</style>
+
